662c2aad8d338b5d2dc0f804ca1909cfa710db37
Anton Saykin <Saikin.ao@phystech.edu>
2022-12-09 16:10:02 +0000

    auth routes

---
+++ source/arm-operator/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+from backend.routes import auth_routes, index_routes
+from global_variables import db
+
+
+def app_setup(app: Flask):
+    """
+    Entry point to the Flask Server application.
+    """
+    app.secret_key = "secret"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+
+    db.init_app(app)
+
+    app.register_blueprint(index_routes.main)
+    app.register_blueprint(auth_routes.auth)
+    return app
+
+
+app = Flask(
+    __name__, static_folder="frontend/static", template_folder="frontend/templates"
+)
+app_setup(app)
---
+++ source/arm-operator/backend/models.py
@@ -0,0 +1,10 @@
+from global_variables import db
+
+
+class User(db.Model):
+    id = db.Column(
+        db.Integer, primary_key=True
+    )  # primary keys are required by SQLAlchemy
+    login = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    # name = db.Column(db.String(1000))
